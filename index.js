e137c72da6bdd017830158be721f367d54a277c6
Azim-Ahmed <azimuahmed13@gmail.com>
2020-10-05 18:06:36 +0600

    network server finished

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const app = express()
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
 const port = 4000
@@ -26,60 +25,54 @@ client.connect(err => {
   const AllvolunteeringCollection = client.db("volunteerNetwork").collection("userTask");
   const volunteerCollection = client.db("volunteerNetwork").collection("signedInVolunteer");
   console.log("suceess");
- 
-  app.post('/addAllVolunteers', (req, res) => {
-    const DBVolunteers = req.body;
-    console.log(DBVolunteers);
-   
-    AllvolunteeringCollection.insertMany(DBVolunteers)
-    .then(result => {
-        console.log(result.insertedCount);
-        res.send(result.insertedCount) 
-    })
-    })
 
-    app.get('/volunteers', (req, res)=> {
-      AllvolunteeringCollection.find({})
-      .toArray((err, documents)=>{
+  app.post('/addVolunteer', (req, res) => {
+    const singleVolunteer = req.body;
+    AllvolunteeringCollection.insertOne(singleVolunteer)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.get('/volunteers', (req, res) => {
+    AllvolunteeringCollection.find({})
+      .toArray((err, documents) => {
         res.send(documents)
       })
-    })
+  })
 
-    app.post('/addVolunteer', (req, res) => {
-      const volunteer = req.body;
-      volunteerCollection.insertOne(volunteer)
-      .then(result=> {
-        res.send(result.insertedCount) 
+  app.post('/addSingleVolunteer', (req, res) => {
+    const volunteer = req.body;
+    volunteerCollection.insertOne(volunteer)
+      .then(result => {
+        res.send(result.insertedCount > 0)
       })
-    })
-    app.get('/involvedvolunteer', (req, res) => {
-      // console.log(req.query.email);
-      volunteerCollection.find({})
-      .toArray((err,documents) => {
+  })
+  app.get('/involvedvolunteerforadmin', (req, res) => {
+    volunteerCollection.find({})
+      .toArray((err, documents) => {
+        res.status(200).send(documents)
+      })
+  })
+  app.get('/involvedvolunteer', (req, res) => {
+    volunteerCollection.find({ email: req.query.email })
+      .toArray((err, documents) => {
         res.send(documents)
       })
-    })
-    app.get('/involvedvolunteer', (req, res) => {
-      // console.log(req.query.email);
-      volunteerCollection.find({email : req.query.email})
-      .toArray((err,documents) => {
-        res.send(documents)
-      })
-    })
+  })
 
-    app.delete('/delete', (req, res)=>{
-      console.log(req.query.id);
-      volunteerCollection.deleteOne({_id: ObjectId(req.query.id)})
-      .then(result =>{
+  app.delete('/delete', (req, res) => {
+    volunteerCollection.deleteOne({ _id: ObjectId(req.query.id) })
+      .then(result => {
         res.send(result.deletedCount > 0)
       })
-    })
-    
+  })
+
 });
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).send('Hello World!')
 })
 
 app.listen(port)
